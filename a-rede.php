7a6a2517d079149b515d6d93413307cbbac8be48
rodrigo-mamangava <rodrigo@mamangava.com>
2016-07-20 14:47:21 -0300

    pg a rede

---
+++ a-rede.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Rede Abrolhos</title>
+
+        <!-- Bootstrap -->
+        <link href="css/main.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <header>
+            <?php include './template-parts/faixa-verde-top.php'; ?>
+            <?php include './template-parts/nav-bar.php'; ?>
+        </header>
+
+
+
+        <div id="rede">
+
+            <div class="container">
+                <div class="row"> 
+                    <div class="col-xs-12">
+
+
+
+                            <h1>A Rede Abrolhos</h1>
+
+                            <p>
+                                Além de contribuir para o estabelecimento de patamares 
+                                de referência e avaliações de tendências em áreas recifais 
+                                sob condicionantes ecológicas dissimilares e regimes de manejo 
+                                diferenciados, utilizando modelos diversos (e.g. microbiota, 
+                                comunidades bentônicas, peixes recifais), a Rede Abrolhos tem 
+                                desenvolvido mecanismos de Planejamento Sistemático 
+                                para Conservação (PSC) no sentido de determinar 
+                                quais são as áreas-chave para a biodiversidade 
+                                e as lacunas de conservação na região de Abrolhos, 
+                                incluindo a modelagem de cenários para o 
+                                estabelecimento de novas áreas de proteção e 
+                                manejo, levando em consideração as principais 
+                                atividades econômicas que dependem (e.g. pesca) 
+                                e influenciam (e.g. mineração) a biodiversidade.
+                            </p>
+
+                            <h2>As metas centrais da Rede Abrolhos incluem:</h2>
+
+                            <ol>
+                                <li><span>
+                                    consolidação de um núcleo virtual de referência 
+                                    sobre os sistemas recifais de Abrolhos, cujo 
+                                    principal produto é um sistema de gestão da 
+                                    informação integrando atores de diversos 
+                                    setores;
+                                    </span></li>
+                                <li><span>produção de uma síntese acerca da diversidade 
+                                    taxonômica e funcional no sistema recifal de 
+                                    Abrolhos;</span></li>
+                                <li><span>consolidação do mapeamento de habitats bênticos, 
+                                    resultando em mapas temáticos para aplicações 
+                                    na área de gestão ambiental, inclusive a 
+                                    criação de novas áreas marinhas protegidas;</span></li>
+                                <li><span>compreensão dos fatores que influenciam a 
+                                    produtividade e a dinâmica nos diversos 
+                                    compartimentos do sistema recifal de Abrolhos, 
+                                    gerando recomendações para o manejo das áreas 
+                                    recifais, dentro e fora de unidades de 
+                                    conservação;</span></li>
+                                <li><span>descrição do ciclo reprodutivo e principais 
+                                    parâmetros populacionais das principais 
+                                    espécies de peixes recifais alvo da pesca, 
+                                    subsidiando medidas específicas de manejo;</span></li>
+                                <li><span>estabelecimento das taxas de crescimento e 
+                                    de mineralização de CaCO3 das principais 
+                                    espécies de corais e algas coralináceas, bem 
+                                    como a análise da tolerância de organismos 
+                                    construtores (algas calcárias e corais) a 
+                                    diferentes condições ambientais, gerando 
+                                    elementos essenciais para o entendimento e 
+                                    previsão de respostas às mudanças climáticas.</span></li>
+                            </ol>
+                            
+                            <h1>A Rede Abrolhos</h1>
+                            
+                            <p>Integram a Rede, pesquisadores do Brasil e do 
+                                exterior, além de estudantes de doutorado, 
+                                mestrado e iniciação científica que atuam de 
+                                maneira integrada. As principais áreas de 
+                                atuação da equipe da rede são: mapeamento de 
+                                habitats, biodiversidade, ecologia recifal, 
+                                paleoecologia e gestão da informação. </p>
+                            
+                            <a class="btn-saiba-mais">
+                                conheça a equipe
+                            </a>
+                            
+                            <h1>Expedições</h1>
+                            
+                            <p>
+                                Atividades realizadas em expedições recentes.
+                            </p>
+                            
+                            <a class="btn-saiba-mais">
+                                saiba mais
+                            </a>
+                            
+                            <h1>Projetos</h1>
+                            
+                            <p>
+                                Principais projetos em execução atualmente.
+                            </p>
+                            
+                            <a class="btn-saiba-mais">
+                                conheça os projetos
+                            </a>
+                            
+                            <h1>Financiamento</h1>
+                            
+                            
+
+           
+
+
+                    </div>
+                </div>
+            </div>
+        </div><!-- #contato -->
+
+        <footer>
+            <?php include './template-parts/faixa-verde-bottom.php'; ?>            
+            <?php include './template-parts/faixa-branca-bottom.php'; ?>            
+            <?php include './template-parts/faixa-verde-bottom-2.php'; ?>
+        </footer>
+
+    </body>
+</html>
--- template-parts/nav-bar.php
+++ template-parts/nav-bar.php
@@ -17,7 +17,7 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li><a href="rede.php">A Rede</a></li>
+                    <li><a href="a-rede.php">A Rede</a></li>
                     <li><a href="#">Eixo temático</a></li>
                     <li><a href="#">Publicações</a></li>
                     <li><a href="#">Notícias</a></li>
